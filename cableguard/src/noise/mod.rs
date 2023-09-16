37f134427218d34014de1414168579308fc18f40
“alanesmizi” <canal@frankevych.com>
2023-09-16 07:29:44 +0000

    on screen confirmation

--- cableguard/src/noise/mod.rs
+++ cableguard/src/noise/mod.rs
@@ -1040,10 +1040,10 @@ let i64_timestamp = string_timenow.expect("Error: Can't parse near block timesta
 let naivedatetime_timestamp = NaiveDateTime::from_timestamp_opt(i64_timestamp, 0);
 
 if naivedatetime_timestamp <= Some(naivedatetime_notafter) && naivedatetime_timestamp >= Some(naivedatetime_notbefore) {
-    println!("The current datetime is within the specified range.");
+    println!("Info: The current datetime {} is within {} and {}", naivedatetime_timestamp,naivedatetime_notbefore,naivedatetime_notafter);
     return true
 } else {
-    println!("The current datetime is outside the specified range.");
+    println!("Error: The current datetime {} is NOT within {} and {}", naivedatetime_timestamp,naivedatetime_notbefore,naivedatetime_notafter);
     return false
 }
 } 
