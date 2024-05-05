d9966f9ec8c7c916a5a0bc26155bb8d575923f08
“alanesmizi” <canal@frankevych.com>
2024-05-05 16:40:55 +0000

    1.0.5 cgcstart.sh

--- cgsh/cgcstart.sh
+++ cgsh/cgcstart.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-VERSION="1.0.testnet"
-#export NFTCONTRACTID=$(cat /home/icarus24/cgwallet/account)
+#minor version is odd for testnet, even for mainnet
+VERSION="1.0.5"
+#export NFTCONTRACTID=$(cat ~/cgtun/cgsh/account)
 echo "Version" $VERSION "running on " $BLOCKCHAIN_ENV "at Smart Contract" $NFTCONTRACTID " Get help with: "$0" help"
 
 # Check if there are no entry parameters
 if [ $# -eq 0 ]; then
-    echo "Error: No entry parameter provided. Usage: $0 <json_file_name>"
+    echo "Error: No entry parameter provided. Usage:" $0 "<json_file_name> (without extension)"
     exit 1
+fi
+
+if [ "$1" == "help" ]; then
+    echo "Usage: "$0" [account_id] [Options]"
+    echo "Works best when called from the cgtun directory"
+    echo ""
+    echo "Options:"
+    echo "  "$0" <json_file_name> (without extension)"
+    exit 0
 fi
 
 # Check if the JSON file exists
@@ -18,7 +28,7 @@ if [ ! -f "$json_file" ]; then
 fi
 
 # Run cableguard and start the tunnel
-if /usr/bin/sudo /usr/bin/cableguard-cli -f -v trace "$json_file" >> ~/cableguard.$1.log 2>&1; then
+if sudo ./target/release/cableguard-cli -v trace $json_file >> ~/cableguard.$1.log 2>&1; then
     echo "cableguard-cli: Started and created the tunnel."
 else
     echo "Error: cableguard-cli failed to start."
@@ -27,11 +37,12 @@ fi
 
 # Run `sudo wg show` and capture the interface name
 interface_name=$(sudo wg show | awk '/^interface:/ {print $2}')
+echo $interface_name
 
 # Check if the interface name is not empty
 if [ -n "$interface_name" ]; then
     # Update bring the interface up
-    if /usr/bin/sudo ip link set "$interface_name" up  >> /var/log/cableguard.log 2>&1; then
+    if sudo ip link set "$interface_name" up  >> ~/cableguard.$1.log 2>&1; then
         echo "Bringing up interface: '$interface_name'."
     else
         echo "Error: Could not bring interface up"
@@ -39,14 +50,14 @@ if [ -n "$interface_name" ]; then
     fi
 
     # Update iptables rules
-    if /usr/bin/sudo ip route add 0.0.0.0/1 dev "$interface_name" >> /var/log/cableguard.log 2>&1; then
+    if sudo ip route add 0.0.0.0/1 dev "$interface_name" >> ~/cableguard.$1.log 2>&1; then
         echo "Default Gateway 0.0.0.0/1 rule: Added for interface '$interface_name'."
     else
         echo "Error: Failed to add iptables FORWARD rule."
         exit 1
     fi
 
-    if /usr/bin/sudo ip route add 128.0.0.0/1 dev "$interface_name" >> /var/log/cableguard.log 2>&1; then
+    if sudo ip route add 128.0.0.0/1 dev "$interface_name" >> ~/cableguard.$1.log 2>&1; then
         echo "Default Gateway 128.0.0.0/1 rule: Added for interface '$interface_name'."
     else
         echo "Error: Failed to add iptables NAT rule."
