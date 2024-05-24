d68a4f577df111e1b3c41999d025b2ad5fbad346
alanesmizi <canal@frankevych.com>
2024-05-24 15:42:54 +0000

    2 testing expect instead of unwrap

--- cableguard/src/device/mod.rs
+++ cableguard/src/device/mod.rs
@@ -468,7 +468,8 @@ impl Device {
             Ok(serviceprovider_rodt) => {
                 let mut peer_port: u16 = 0;
                 let dnssecresolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
-                let ipresponse = dnssecresolver.lookup_ip(serviceprovider_rodt.metadata.subjectuniqueidentifierurl.clone()+".").expect("Error: VPN DNS entry not found");
+                let ipresponse = dnssecresolver.lookup_ip(serviceprovider_rodt.metadata.subjectuniqueidentifierurl.clone()+".").expect("Error: VPN DNS entry not found.
+                    A DNS entry with the IP address of the VPN server configured in the RODiT must be accessible");
                 let ipaddress = ipresponse.iter().next().expect("Error: No IP address found for subdomain");   
                 let cfgresponse = dnssecresolver.txt_lookup(serviceprovider_rodt.metadata.subjectuniqueidentifierurl.clone()+".");
                 cfgresponse.iter().next().expect("Error: No VPN Server Public Key found!");
